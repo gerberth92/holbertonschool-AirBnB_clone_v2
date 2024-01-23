f08bbe352384aae2885b97597b0fb0dd2cbaaed2
gerberth92 <gerberthcontreras@gmail.com>
2024-01-23 12:36:29 -0500

    task 3

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""Este modulo define las rutas con flask"""
+from flask import Flask
+from markupsafe import escape
+
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def home():
+    """Esta funcion define la ruta home"""
+    return ("Hello HBNB!")
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """Esta funcion define la ruta /hbnb"""
+    return ("HBNB")
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c(text):
+    """Captura una string ingresada por el usuario"""
+    if "_" in text:
+        text = text.replace("_", " ")
+        return ("C {}".format(escape(text)))
+    else:
+        return ("C {}".format(escape(text)))
+
+
+@app.route("/python/<text>", strict_slashes=False)
+def python(text="is cool"):
+    if "_" in text:
+        text = text.replace("_", " ")
+        return ("Python {}".format(escape(text)))
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
