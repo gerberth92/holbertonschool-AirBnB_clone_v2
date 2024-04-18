4a47d42edf1a8b31992f1ad3238ebd8038475df5
gerberth92 <gerberthcontreras@gmail.com>
2024-04-18 15:00:34 -0500

    add comment

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,9 +1,14 @@
-""" Este modulo prepara un servidor sql. """
+-- Este modulo prepara un servidor sql.
+
 
 -- crea una base de datos si no existe.
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
-CREATE USER IF NOT EXISTS "hbnb_dev"@"localhost" IDENTIFIED BY "hbnb_dev_pwd";
-GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO "hbnb_dev"@"localhost";
-GRANT SELECT ON performance_schema.* TO "hbnb_dev"@"localhost";
+-- crea un usuario si no existe.
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+-- otorga privilegios a un usuario.
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+-- otorga un privilegio.
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
+-- recarga los privilegios.
 FLUSH PRIVILEGES;
 
