2ab6fb98266a6f3839a129b0ab0edeffeeab3a76
gerberth92 <gerberthcontreras@gmail.com>
2024-04-18 14:42:04 -0500

    prepara un servidor sql

---
+++ setup_mysql_dev.sql
@@ -0,0 +1,8 @@
+""" Este modulo prepara un servidor sql. """
+
+-- crea una base de datos si no existe.
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+CREATE USER IF NOT EXISTS "hbnb_dev"@"localhost" IDENTIFIED BY "hbnb_dev_pwd";
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO "hbnb_dev"@"localhost";
+GRANT SELECT ON performance_schema.* TO "hbnb_dev"@"localhost";
+
