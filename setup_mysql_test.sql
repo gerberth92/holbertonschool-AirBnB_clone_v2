41c1900555f8fe99263ce29d6a2e54ddfca3d300
gerberth92 <gerberthcontreras@gmail.com>
2024-04-18 15:19:03 -0500

    mejorando el stilo

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -5,9 +5,9 @@
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
 -- crea un usuario si no existe.
 CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
---otorgando privilegios.
+-- otorgando privilegios.
 GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
---otorgando un solo privilegio.
+-- otorgando un solo privilegio.
 GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
 -- recargando los privilegios.
 FLUSH PRIVILEGES;
