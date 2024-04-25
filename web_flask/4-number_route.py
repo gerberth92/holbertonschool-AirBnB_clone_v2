d19ef25d165bdb884ded6291c62f6e48bafaa3ac
gerberth92 <gerberthcontreras@gmail.com>
2024-04-24 22:44:31 -0500

    task 4

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+""" Este modulo inicia una aplicacion con flask. """
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def home():
+    """ Esta funsion retorna un mensaje. """
+
+    return ("Hello HBNB!")
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """ Esta funcion retorna un mensaje """
+
+    return ("HBNB")
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c(text):
+    """ Esta funcion retorna un valor de una variable. """
+
+    if '_' in text:
+        return ("{} {}". format("C", text.replace('_', ' ')))
+    else:
+        return ("{} {}". format("C", text))
+
+
+@app.route("/python", defaults={"text": "is cool"}, strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def python(text):
+    """ Esta funcion tiene una variable con valor por defecto. """
+
+    if '_' in text:
+        return ("{} {}". format("Python", text.replace('_', ' ')))
+    else:
+        return ("{} {}". format("Python", text))
+
+
+@app.route("/number/<int:n>", strict_slashes=False)
+def number(n):
+    return ("{} {}". format(n, "is a number"))
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
