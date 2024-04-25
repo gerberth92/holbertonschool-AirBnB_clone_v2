bd1526a832e0b2b77fa0e2334b8a2c7c48ab7a9d
gerberth92 <gerberthcontreras@gmail.com>
2024-04-24 21:00:03 -0500

    task 0

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" Este modulo inicia una aplicacion con flask. """
+
+
+from flask import Flask
+
+
+app = Flask(__name__)
+
+@app.route("/", strict_slashes=False)
+def home():
+    """ Esta funsion retorna un mensaje. """
+    return ("Hello HBNB!")
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
+
